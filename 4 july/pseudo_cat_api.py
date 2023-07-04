bbaa1022a8d2bb3f7a1686167bbc223f707bfe04
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-04 15:45:11 +0300

    pseudo cat download done

--- 4 july/pseudo_cat_api.py
+++ 4 july/pseudo_cat_api.py
@@ -1,2 +1,16 @@
 from random import random
+import asyncio
 
+async def pseudo_download(n: int):
+    print(f'Cat {n} start download')
+    await asyncio.sleep(random())
+    print(f'Cat {n} done!')
+    
+async def main():
+    tasks = []
+    for i in range(10):
+        tasks.append(pseudo_download(i))
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
+    
