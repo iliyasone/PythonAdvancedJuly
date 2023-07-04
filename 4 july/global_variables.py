694282e3335d2a40b7662c1154c08ae920db04f6
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-04 10:50:32 +0300

    decoratots begining

---
+++ 4 july/global_variables.py
@@ -0,0 +1,17 @@
+IMPORTANT_ID = 12233125
+
+
+def send_message(message: str):    
+    print(f'Сообщение было отправлено пользователю {IMPORTANT_ID}: ' + message)
+    
+    
+    if False:
+        IMPORTANT_ID = 34
+    
+        
+
+
+send_message("тест")
+    
+
+    
