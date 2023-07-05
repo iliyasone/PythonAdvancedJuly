15ba10204247dedd075d067eda835ae5ec0303ab
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-05 10:10:08 +0300

    cat api in one file

--- 4 july/cat_api.py
+++ 4 july/cat_api.py
@@ -2,4 +2,19 @@ import requests
 
 url = 'https://api.thecatapi.com/v1/images/search?limit=10'
 
-requests.get(url)
+def main():
+    response = requests.get(url).json()
+
+    url_cats = []
+    for data in response:
+        url_cats.append(data['url'])
+        
+    for i in range(10):
+        file = open(f'cat{i}.jpg', 'wb')
+        file.write(
+            requests.get(url_cats[i]).content
+            )
+        file.close()
+        
+if __name__ == "__main__":
+    main()
