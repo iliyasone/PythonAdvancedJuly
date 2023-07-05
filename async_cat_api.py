c343fb11fee28d59ce6720e9dd6acc4df3d0357a
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-05 11:05:45 +0300

    using with statemets

--- async_cat_api.py
+++ async_cat_api.py
@@ -11,32 +11,31 @@ async def download(session: aiohttp.ClientSession,
     response = await session.get(url)
     response_bytes = await response.read()
     
-    file = open(f'cats/cat{n}.jpg', 'wb') # write bytes
-    file.write(response_bytes)
-    file.close()
+
+    with open(f'cats/cat{n}.jpg', 'wb') as file:
+        file.write(response_bytes)
+    
     
     print(f'Cat {n} done!')
     
-async def main():
-    session = aiohttp.ClientSession()
-    
-    response = await session.get(url)
-    response_json = await response.json()    
+async def main():    
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        response_json = await response.json()    
 
-    url_cats = []
-    for data in response_json:
-        url_cats.append(data['url'])
+        url_cats = []
+        for data in response_json:
+            url_cats.append(data['url'])
 
 
-    tasks = []
-    for i in range(10):
-        tasks.append(download(session, url_cats[i], i))
-    await asyncio.gather(*tasks)
-    
-    response.release()
-
-    await session.close()
-
+        tasks = []
+        for i in range(10):
+            tasks.append(download(session, url_cats[i], i))
+        await asyncio.gather(*tasks)
+        
+        response.release()
+        
+        
 @time_measure_decorator
 def sync_main():
     asyncio.run(main())
