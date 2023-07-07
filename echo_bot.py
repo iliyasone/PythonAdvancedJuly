741df2f7742a34181b32bdea2100627c10513a54
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-07 11:55:00 +0300

    states example

--- echo_bot.py
+++ echo_bot.py
@@ -2,12 +2,31 @@ from config import TOKEN
 
 from aiogram import Bot, Dispatcher, executor, types
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 
-@dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
-    """Ловец (обработчик сообщений)"""
-    await message.answer("Добро пожаловать в тестового эхо бота!")
-    
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'], state='*')
+async def start_handler(message: types.Message, contex: FSMContext):
+    """Ловец start сообщения"""
+    await message.answer("Добро пожаловать в групповой анонимный чат")
+    await contex.set_state("name")
+
+
+@dp.message_handler(state='name')
+async def name_asking(message: types.Message, contex: FSMContext):
+    name = message.text
+    contex.update_data({"name" : name})
+    await message.reply(f"Приятно познакомиться, {name}. Ты теперь в чате!")
+    await contex.set_state("g_chatting")
+
+
+@dp.message_handler(state='g_chatting')
+async def echo_handler(message: types.Message):
+    """Эхо-ловец"""
+    await message.answer(message.text)
+
 executor.start_polling(dp,  skip_updates=True)
