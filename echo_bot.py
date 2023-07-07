3ddf13a89b369e1b849046e1326515fe042cbb85
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-07 11:59:06 +0300

    fix contex name

--- echo_bot.py
+++ echo_bot.py
@@ -10,18 +10,18 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'], state='*')
-async def start_handler(message: types.Message, contex: FSMContext):
+async def start_handler(message: types.Message, state: FSMContext):
     """Ловец start сообщения"""
-    await message.answer("Добро пожаловать в групповой анонимный чат")
-    await contex.set_state("name")
+    await message.answer("Добро пожаловать в групповой анонимный чат. Напиши своё имя")
+    await state.set_state("name")
 
 
 @dp.message_handler(state='name')
-async def name_asking(message: types.Message, contex: FSMContext):
+async def name_asking(message: types.Message, state: FSMContext):
     name = message.text
-    contex.update_data({"name" : name})
+    state.update_data({"name" : name})
     await message.reply(f"Приятно познакомиться, {name}. Ты теперь в чате!")
-    await contex.set_state("g_chatting")
+    await state.set_state("g_chatting")
 
 
 @dp.message_handler(state='g_chatting')
