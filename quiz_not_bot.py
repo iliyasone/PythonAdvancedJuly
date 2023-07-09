13d2ea7adbee3eb290b2cea4b288dc93910c075b
iliyastwo <i.dzhabbarov@innopolis.innopolis>
2023-07-09 14:45:49 +0300

    quiz not bot done with dataclasses

--- quiz_not_bot.py
+++ quiz_not_bot.py
@@ -1,31 +1,48 @@
-questions = [
-    {'text': "Сколько было найдено планет "
-             "за пределами Солнечной системы?",
-     'answers': ["< 1k", "1k - 10k", "10k - 1kk", "> 1kk",],
-     'rigth_answer_index': 1,
-     'explanation' : None    
-    },
+from dataclasses import dataclass
+
+
+@dataclass
+class Question:
+    text: str
+    answers: list[str]
+    rigth_answer_index: int
+    explanation: str | None = None
+    rigth_answers_indexes: list[int] | None = None
     
-    {'text': "Сколько лет Cолнцу?",
-     'answers': ["< 1k", "1k - 10k", "10k - 1kk", "> 1kk",],
-     'rigth_answer_index': 3,
-     'explanation' : None    
-    }
+
+    @property
+    def rigth_answer(self):
+        return self.answers[self.rigth_answer_index]
+
+questions = [
+    Question(text = "Сколько было найдено планет за пределами Солнечной системы?",
+            answers=["< 1k", "1k - 10k", "10k - 1kk", "> 1kk",],
+            rigth_answer_index=1,
+            explanation='Планеты искать сложно'),
+    Question(text = "Сколько лет Земле?",
+            answers=["2023", "8000", "6.5ккк", "Земли не существует"],
+            rigth_answer_index=1),
+    Question(text = "Солнце является ...",
+            answers=["Белым карликом", "Желтым карликом", "Солнца не сущесвует", "Красным гигантом"],
+            rigth_answer_index=1),
+
 ]
 
-for question in questions:
-    print(question['text'])
-    print(*question['answers'],sep='\n')
-    answer_from_user = input()
-    rigth_answer_index = question['rigth_answer_index']
-    if answer_from_user != question['answers'][rigth_answer_index]:
-        print(f"Неправильно! Правильный ответ {question['answers'][rigth_answer_index]}")
 
+def main():
+    from time import sleep
 
-
-question = {
-    'text' : 'текст вопроса',
-    'answers': ['а', 'б', 'в'],
-    'right_answer_index' : 0,
-    'explanation' : 'объяснение'
- }
+    for question in questions:
+        print(question.text)
+        print(*question.answers, sep='\n')
+        answer = input('Введите ваш ответ: ')
+        sleep(0.5)
+        if answer != question.rigth_answer:
+            print('Неправильный ответ')
+            print(f'правильный ответ {question.rigth_answer}')
+            if question.explanation:
+                print(question.explanation)
+        sleep(5)
+        
+if __name__ != '__main__':
+    main()
